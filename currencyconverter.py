3aef5853aadf7dc1cc0ba75f3f68d5e5f051d3e5
RogerSellin <sellinroger@gmail.com>
2022-10-12 00:39:09 +0200

    Add: combobox for currency

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import ttk
 
 
 from currency_converter import CurrencyConverter
@@ -9,6 +9,7 @@ root = tk.Tk()
 
 root.title("Converter")
 
+root.resizable(False, False)
 
 window_width = 600
 window_height = 400
@@ -24,6 +25,10 @@ center_y = int(screen_height/2 - window_height / 2)
 # set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
+current_var = tk.StringVar()
+from_currency = ttk.Combobox(root, textvariable=current_var)
+from_currency['values'] = ('value1', 'value2', 'value3')
+from_currency.pack(fill=tk.X, padx=5, pady=5)
 
 
 
