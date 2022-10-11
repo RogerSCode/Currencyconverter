14bc8d5119bd35d17bf3da80db90a03a6ffcfd04
RogerSellin <sellinroger@gmail.com>
2022-10-11 22:18:38 +0200

    Update: rootwindow size and centered on screencenter

--- currencyconverter.py
+++ currencyconverter.py
@@ -9,4 +9,22 @@ root = tk.Tk()
 
 root.title("Converter")
 
+
+window_width = 600
+window_height = 400
+
+# get the screen dimension
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# find the center point
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+
+# set the position of the window to the center of the screen
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+
+
+
 root.mainloop()
