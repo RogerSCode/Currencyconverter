d0513a06be8626318a0bb6aae358ac795a5bfb54
RogerSellin <sellinroger@gmail.com>
2022-10-12 00:52:41 +0200

    Add: Labels to describe what the comboboxes are for

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-
+from tkinter.ttk import Label
 
 from currency_converter import CurrencyConverter
 
@@ -25,14 +25,21 @@ center_y = int(screen_height/2 - window_height / 2)
 # set the position of the window to the center of the screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
+label1 = Label(root, text='Convert from:')
+label1.pack(ipadx=10, ipady=10)
+
 from_current_var = tk.StringVar()
 from_currency = ttk.Combobox(root, textvariable=from_current_var)
-from_currency['values'] = ('value1', 'value2', 'value3')
+from_currency['values'] = ('value1', 'value2', 'value3') # TODO insert real currency 
 from_currency.pack(fill=tk.X, padx=5, pady=5)
+
+
+label2 = Label(root, text='Convert to:')
+label2.pack(ipadx=10, ipady=10)
 
 to_current_var = tk.StringVar()
 to_currency = ttk.Combobox(root, textvariable=to_current_var)
-to_currency['values'] = ('value1', 'value2', 'value3')
+to_currency['values'] = ('value1', 'value2', 'value3') # TODO insert real currency 
 to_currency.pack(fill=tk.X, padx=5, pady=5)
 
 root.mainloop()
